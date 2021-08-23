2d3b1284f7e1eabd8c889c7a86eb3a6ed4a23b95
HoanNg1999 <hoan.nguyen@selu.edu>
2021-08-23 00:00:21 -0500

    New Effect: DisableOverScroll for HoanGames.Android

---
+++ HoanGames.Android/DisabledOverScrollEffect.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HoanGames.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("HoanGames")]
+[assembly: ExportEffect(typeof(DisabledOverScrollEffect), "DisabledOverScroll")]
+namespace HoanGames.Droid
+{
+    class DisabledOverScrollEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                Control.OverScrollMode = OverScrollMode.Never;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ HoanGames/Effects/DisabledOverScroll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HoanGames.Effects
+{
+    public class DisabledOverScroll : RoutingEffect
+    {
+        public DisabledOverScroll() : base("HoanGames.DisabledOverScroll")
+        {
+        }
+    }
+}
